6028cc0d05eee1b4c209187a993383928493d14c
Doritos0 <meyesblue@gmail.com>
2026-07-22 12:47:24 -0400

    Agregar tablas iniciales del proceso

---
+++ backend/core/choices.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class EstadoCRM(models.TextChoices):
+    COMPROMETIDO = "comprometido", "Comprometido"
+    SIN_COMPROMISO = "sin_compromiso", "Sin compromiso"
+    PAGADO = "pagado", "Pagado"
+
+class TipoPago(models.TextChoices):
+    TOTAL = "total", "Total"
+    PARCIAL = "parcial", "Parcial"
+
+class Situacion(models.TextChoices):
+    PENDIENTE = "pendiente", "Pendiente",
+    VALIDADO = "validado", "Validado"
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from .choices import EstadoCRM, TipoPago, Situacion
 # Create your models here.
+
+class CRMFila(models.Model):
+    id = models.AutoField(primary_key=True)
+    credito_id = models.IntegerField()
+    fecha_contacto = models.DateField(null=True)
+    fecha_compromiso = models.DateField(null=True)
+    fecha_pago = models.DateField(null=True)
+    estado = models.CharField(max_length=20, choices=EstadoCRM.choices, default=EstadoCRM.SIN_COMPROMISO)
+    pago = models.CharField(max_length=20, choices=TipoPago, null=True)
+    situacion = models.CharField(max_length=20, choices=Situacion, null=True)
+    cuotas = models.IntegerField()
+    monto = models.IntegerField()
+
+    class Meta:
+        db_table = 'crm_fila'
+
+#CREDITO
+class Credito(models.Model):
+    id = models.AutoField(primary_key=True)
+    rut_deudor = models.CharField()
+
+    class Meta:
+        db_table = 'credito'
+
+# CUOTA -> HEREDA CREDITO_ID
+class Cuota(models.Model):
+    id = models.AutoField(primary_key=True)
+    credito_id = models.ForeignKey(Credito, on_delete=models.CASCADE, db_column='credito_id', related_name='cuotas')
+    fecha = models.DateField()
+    monto = models.IntegerField()
+
+    class Meta:
+        db_table = 'cuota'
