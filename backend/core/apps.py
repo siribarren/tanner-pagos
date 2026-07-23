33f9fc2028c2fd604d0e719ffb934fa0f5fb187d
Doritos0 <meyesblue@gmail.com>
2026-07-23 11:20:31 -0400

    Agregar corrección de la configuración de logging

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self) -> None:
+        from core.config.log_config import configurar_log
+
+        configurar_log()
